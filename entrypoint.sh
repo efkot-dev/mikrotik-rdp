48cf670ed2fe40896b91beaf9bdf30f86f1299d6
efkot-dev <fggfrew@gmail.com>
2026-09-24 23:58:22 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Создаем пользователя если не существует
+# Создаем пользователя
 if ! id rdpuser >/dev/null 2>&1; then
     adduser -D -s /bin/sh rdpuser
 fi
@@ -10,13 +10,17 @@ fi
 echo "rdpuser:${RDP_PASSWORD}" | chpasswd
 
 # Создаем необходимые директории
-mkdir -p /run/dbus /var/run/xrdp
+mkdir -p /run/dbus /var/run/xrdp /tmp/.X11-unix
+chmod 1777 /tmp/.X11-unix
 
 # Запускаем dbus
 dbus-daemon --system --fork
 
-# Запускаем xrdp-sesman в фоне
-/usr/sbin/xrdp-sesman
+# Запускаем xrdp-sesman
+/usr/sbin/xrdp-sesman &
+
+# Ждем запуска sesman
+sleep 2
 
 # Запускаем xrdp в foreground
 exec /usr/sbin/xrdp --nodaemon
